dd4a0cbeff09a6c44abe8901d5beec3a1fcc8410
NonlmmemorMei <nonimmemormei@hotmail.com>
2013-03-17 20:16:24 +0400

    second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <math.h>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    long long l, r, m; cin >> l >> r;
+    m = l ^ r;
+    long long count = 1;
+    while(count <= m)
+    count = count << 1;
+    cout << (count - 1) << endl;
     return 0;
 }
